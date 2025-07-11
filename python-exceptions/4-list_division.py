b18c38c83b7379656a8f6c4e64d919b49732dc7c
Teniolaaaa <t.olaleye@alustudent.com>
2025-07-11 20:13:37 +0200

    Update 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
@@ -8,10 +9,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-        except (TypeError, ValueError):
+        except (ValueError, TypeError):
             print("wrong type")
             result.append(0)
         except IndexError:
             print("out of range")
             result.append(0)
+        finally:
+            continue
     return result
