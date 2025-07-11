9c153186fc483f4e520b3d71c872851c01f1418a
Teniolaaaa <t.olaleye@alustudent.com>
2025-07-11 23:20:47 +0200

    Update 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-"""This defines the Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-class Square(Rectangle):
-    """To represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of a new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+def is_same_class(obj, a_class):
+    """Check if obj is exactly instance of a_class"""
+    return type(obj) is a_class
