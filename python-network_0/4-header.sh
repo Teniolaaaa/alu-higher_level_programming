b8325a9dfc23db2eb0a8991ae9317527e12e3a72
Teniolaaaa <t.olaleye@alustudent.com>
2025-07-23 17:07:58 +0200

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL as an argument, sends a GET request with custom header, and displays the body of the response
+# Script that takes in a URL, sends a GET request with a custom header, and displays the body of the response
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
-
