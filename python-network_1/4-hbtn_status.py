02a3d680482a4c2884d101c451212e92a55b02f9
Teniolaaaa <t.olaleye@alustudent.com>
2025-08-02 09:25:43 +0100

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests and prints response details."""
+"""Fetches https://alu-intranet.hbtn.io/status using requests and prints the response body details."""
 
 import requests
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+# Your code here
